922215a5b501640021bd0236e2dc9a005d2d8601
Lucas <lcasagrande@itba.edu.ar>
2017-08-18 18:42:20 -0300

    hello snake

---
+++ Assets/Tail.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tail : MonoBehaviour {
+	public GameObject p;
+	public int moveSpeed = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (p != null) {
+			//Debug.Log (p);
+			//Debug.Log (transform.position);
+			transform.position += moveSpeed * Time.deltaTime * (p.transform.position - transform.position);
+		}
+		//transform.position += moveSpeed * Time.deltaTime * transform.right;
+	}
+
+}
